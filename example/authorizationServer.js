5c150582114ed84645bcb115ab7c2e2a64b78574
Antonio Sanso <asanso@adobe.com>
2015-07-14 11:52:51 +0200

    adding static files and AS

---
+++ example/authorizationServer.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var url = require("url");
+
+var app = express();
+
+app.get("/oauth/authorize", function(req, res){
+
+	var code = "SplxlOBeZQQYbYS6WxSbIA";
+
+	var urlParsed =url.parse(req.query.redirect_uri);
+
+	urlParsed.query = urlParsed.query || {};
+	urlParsed.query.code = code;
+	urlParsed.query.state = req.query.state; 
+
+	res.redirect(url.format(urlParsed));
+});
+
+
+app.post("/oauth/token", function(req, res){
+	res.status(200).json({ access_token: '2YotnFZFEjr1zCsicMWpAA' });
+});
+
+var server = app.listen(8433, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+ 
