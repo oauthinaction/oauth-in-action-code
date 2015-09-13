749db45735070a814f9b373ae50c0de8d3968732
Justin Richer <jricher@mit.edu>
2015-09-13 00:08:31 -0400

    code cleanup

--- exercises/ch-3-ex-1/client.js
+++ exercises/ch-3-ex-1/client.js
@@ -16,10 +16,7 @@ app.set('views', 'files/client');
 // authorization server information
 var authServer = {
 	authorizationEndpoint: 'http://localhost:9001/authorize',
-	tokenEndpoint: 'http://localhost:9001/token',
-	revocationEndpoint: 'http://localhost:9001/revoke',
-	registrationEndpoint: 'http://localhost:9001/register',
-	userInfoEndpoint: 'http://localhost:9001/userinfo'
+	tokenEndpoint: 'http://localhost:9001/token'
 };
 
 // client information
