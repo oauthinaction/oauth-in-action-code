507a592cce23fc3808056001c16d92930e1c1ce9
Justin Richer <jricher@mit.edu>
2015-10-20 14:44:41 -0400

    chapter 4 exercise 1

---
+++ exercises/ch-4-ex-1/protectedResource.js
@@ -0,0 +1,60 @@
+var express = require("express");
+var bodyParser = require('body-parser');
+var cons = require('consolidate');
+var nosql = require('nosql').load('database.nosql');
+var __ = require('underscore');
+var cors = require('cors');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true })); // support form-encoded bodies (for bearer tokens)
+
+app.engine('html', cons.underscore);
+app.set('view engine', 'html');
+app.set('views', 'files/protectedResource');
+app.set('json spaces', 4);
+
+app.use('/', express.static('files/protectedResource'));
+app.use(cors());
+
+var resource = {
+	"name": "Protected Resource",
+	"description": "This data has been protected by OAuth 2.0"
+};
+
+var getAccessToken = function(req, res, next) {
+	/*
+	 * Scan for an access token on the incoming request.
+	 */
+	
+	// REPLACE THESE TWO LINES
+	next();
+	return;
+	// REPLACE THESE TWO LINES
+
+};
+
+app.options('/resource', cors());
+
+
+/*
+ * Add the getAccessToken function to this handler
+ */
+app.post("/resource", cors(), function(req, res){
+
+	/*
+	 * Check to see if the access token was found or not
+	 */
+	
+	// WRAP THIS LINE IN A CONDITIONAL STATEMENT
+	res.json(resource);
+	// WRAP THIS LINE IN A CONDITIONAL STATEMENT
+});
+
+var server = app.listen(9002, 'localhost', function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('OAuth Resource Server is listening at http://%s:%s', host, port);
+});
+ 
