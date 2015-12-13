3627bf1ac1d73e209b7935a194ef9b8319c9794a
Justin Richer <jricher@mit.edu>
2015-12-12 22:22:44 -0500

    chapter 6 exercise 5

---
+++ exercises/ch-6-ex-5/native-client/platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "cordova-plugin-inappbrowser": "1.0.1"
+}
+// BOTTOM OF METADATA
+});
