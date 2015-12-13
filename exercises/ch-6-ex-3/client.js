59cf41fba730a9c5058d4c5518decfeb92f0cddb
Justin Richer <jricher@mit.edu>
2015-12-12 21:56:48 -0500

    chapter 6 exercise 3

---
+++ exercises/ch-6-ex-3/client.js
@@ -0,0 +1,110 @@
+var express = require("express");
+var bodyParser = require('body-parser');
+var request = require("sync-request");
+var cons = require('consolidate');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.engine('html', cons.underscore);
+app.set('view engine', 'html');
+app.set('views', 'files/client');
+
+// authorization server information
+var authServer = {
+	authorizationEndpoint: 'http://localhost:9001/authorize',
+	tokenEndpoint: 'http://localhost:9001/token',
+	revocationEndpoint: 'http://localhost:9001/revoke',
+	registrationEndpoint: 'http://localhost:9001/register',
+	userInfoEndpoint: 'http://localhost:9001/userinfo'
+};
+
+// client information
+
+var client = {
+	"client_id": "oauth-client-1",
+	"client_secret": "oauth-client-secret-1",
+	"redirect_uris": ["http://localhost:9000/callback"],
+	"scope": "openid profile email address phone"
+};
+
+//var client = {};
+
+var protectedResource = 'http://localhost:9002/resource';
+
+var state = null;
+
+var access_token = null;
+var scope = null;
+
+app.get('/', function (req, res) {
+	res.render('index', {access_token: access_token, refresh_token: refresh_token, scope: scope});
+});
+
+app.get('/username_password', function(req, res) {
+	res.render('username_password');
+	return;
+});
+
+app.post('/username_password', function(req, res) {
+	/*
+	 * Implement the resource owner grant type here
+	 */
+	
+	res.render('error', {error: 'Not implemented.'});
+	return;
+});
+
+app.get('/fetch_resource', function(req, res) {
+
+	if (!access_token) {
+		if (refresh_token) {
+			// try to refresh and start again
+			refreshAccessToken(req, res);
+			return;
+		} else {
+			res.render('error', {error: 'Missing access token.'});
+			return;
+		}
+	}
+	
+	console.log('Making request with access token %s', access_token);
+	
+	var headers = {
+		'Authorization': 'Bearer ' + access_token,
+		'Content-Type': 'application/x-www-form-urlencoded'
+	};
+	
+	var resource = request('POST', protectedResource,
+		{headers: headers}
+	);
+	
+	if (resource.statusCode >= 200 && resource.statusCode < 300) {
+		var body = JSON.parse(resource.getBody());
+		res.render('data', {resource: body});
+		return;
+	} else {
+		access_token = null;
+		if (refresh_token) {
+			// try to refresh and start again
+			refreshAccessToken(req, res);
+			return;
+		} else {
+			res.render('error', {error: 'Server returned response code: ' + resource.statusCode});
+			return;
+		}
+	}
+	
+	
+});
+
+app.use('/', express.static('files/client'));
+
+var server = app.listen(9000, 'localhost', function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('OAuth Client is listening at http://%s:%s', host, port);
+});
+ 
