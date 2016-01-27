d43d5d2fddd513fb9d299807a5cfd882f5f27a74
Antonio Sanso <asanso@adobe.com>
2016-01-27 20:51:03 +0100

    adding cors

--- example/chapter8/solution1/protectedResource2.js
+++ example/chapter8/solution1/protectedResource2.js
@@ -156,21 +156,29 @@ app.options('/helloWorld', cors());
 
 app.get("/helloWorld", cors(), getAccessToken, function(req, res){
 	if (req.access_token) {
+		
+		res.setHeader('X-Content-Type-Options','nosniff');
+		res.setHeader('X-XSS-Protection', '1; mode=block');
+
+		var resource = {
+			"greeting" : ""
+		};
+
 		if (req.query.language == "en") {
-			res.send('Hello World');
+			resource.greeting = 'Hello World';
 		} else if (req.query.language == "de") {
-			res.send('Hallo Welt');
+			resource.greeting ='Hallo Welt';
 		} else if (req.query.language == "it") {
-			res.send('Ciao Mondo');
+			resource.greeting = 'Ciao Mondo';
 		} else if (req.query.language == "fr") {
-			res.send('Bonjour monde');
+			resource.greeting = 'Bonjour monde';
 		} else if (req.query.language == "es") {
-			res.send('Hola mundo');
+			resource.greeting ='Hola mundo';
 		} else {
-			res.send("Error, invalid language: "+ req.query.language);
+			resource.greeting = "Error, invalid language: "+ querystring.escape(req.query.language);
 		}
+		res.json(resource);
 	}
-	
 });
 
 var server = app.listen(9002, 'localhost', function () {
