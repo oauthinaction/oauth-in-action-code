3ac36a6b4448f6d04e7308e04031d9c6c9188ac8
Antonio Sanso <asanso@adobe.com>
2016-02-25 12:17:34 +0100

    challenge 80 characters

--- exercises/ch-10-ex-0/completed/authorizationServer.js
+++ exercises/ch-10-ex-0/completed/authorizationServer.js
@@ -143,10 +143,11 @@ app.get("/authorize", function(req, res){
 		}
 		
 		var reqid = randomstring.generate(8);
+		var code_challenge = req.query.code_challenge;
 		
 		requests[reqid] = req.query;
 		
-		res.render('approve', {client: client, reqid: reqid, scope: rscope});
+		res.render('approve', {client: client, reqid: reqid, scope: rscope, code_challenge: code_challenge});
 		return;
 	}
 
@@ -170,7 +171,8 @@ app.post('/approve', function(req, res) {
 			var code = randomstring.generate(8);
 			
 			var user = req.body.user;
-		
+			var code_challenge = req.body.code_challenge;
+
 			var scope = __.filter(__.keys(req.body), function(s) { return __.string.startsWith(s, 'scope_'); })
 				.map(function(s) { return s.slice('scope_'.length); });
 			var client = getClient(query.client_id);
@@ -185,7 +187,7 @@ app.post('/approve', function(req, res) {
 			}
 
 			// save the code and request for later
-			codes[code] = { authorizationEndpointRequest: query, scope: scope, user: user };
+			codes[code] = { authorizationEndpointRequest: query, scope: scope, user: user, code_challenge:code_challenge };
 		
 			var urlParsed = buildUrl(query.redirect_uri, {
 				code: code,
@@ -363,6 +365,14 @@ app.post("/token", function(req, res){
 		if (code) {
 			delete codes[req.body.code]; // burn our code, it's been used
 			if (code.authorizationEndpointRequest.client_id == clientId) {
+
+
+				if (code.code_challenge) {
+					if (code.code_challenge != req.body.code_verifier ) {
+						res.status(400).json({error: 'Invalid code verifier'});
+						return;
+					}
+				}
 
 				var user = userInfo[code.user];
 				if (!user) {		
