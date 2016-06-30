9ecc68fcf7118d136597dc80e084c5b6ab505f0e
Justin Richer <jricher@mit.edu>
2016-06-30 17:46:29 -0400

    major update to 12-2, added client-side management functionality

--- exercises/ch-12-ex-2/completed/client.js
+++ exercises/ch-12-ex-2/completed/client.js
@@ -200,25 +200,79 @@ var registerClient = function() {
 
 app.get('/read_client', function(req, res) {
 
-	/* 
-	 * Read the client's registration information from the management endpoint
-	 */
+	var headers = {
+		'Accept': 'application/json',
+		'Authorization': 'Bearer ' + client.registration_access_token
+	};
+	
+	var regRes = request('GET', client.registration_client_uri, {
+		headers: headers
+	});
+	
+	if (regRes.statusCode == 200) {
+		client = JSON.parse(regRes.getBody());
+		res.render('data', {resource: clien});
+		return;
+	} else {
+		res.render('error', {error: 'Unable to read client ' + regRes.statusCode});
+		return;
+	}
 	
 });
 
 app.post('/update_client', function(req, res) {
 
-	/*
-	 * Update the client's registration with input from the form
-	 */
-
+	var headers = {
+		'Content-Type': 'application/json',
+		'Accept': 'application/json',
+		'Authorization': 'Bearer ' + client.registration_access_token
+	};
+	
+	var reg = __.clone(client);
+	delete reg['client_id_issued_at'];
+	delete reg['client_secret_expires_at'];
+	delete reg['registration_client_uri'];
+	delete reg['registration_access_token'];
+	
+	reg.client_name = req.body.client_name;
+	
+	console.log("Sending updated client: ", reg);
+	
+	var regRes = request('PUT', client.registration_client_uri, {
+		body: JSON.stringify(reg),
+		headers: headers
+	});
+	
+	if (regRes.statusCode == 200) {
+		client = JSON.parse(regRes.getBody());
+		res.render('index', {access_token: access_token, refresh_token: refresh_token, scope: scope, client: client});
+		return;
+	} else {
+		res.render('error', {error: 'Unable to update client ' + regRes.statusCode});
+		return;
+	}
+	
 });
 
 app.get('/unregister_client', function(req, res) {
 
-	/*
-	 * Delete the client's registration from the server
-	 */
+	var headers = {
+		'Authorization': 'Bearer ' + client.registration_access_token
+	};
+	
+	var regRes = request('DELETE', client.registration_client_uri, {
+		headers: headers
+	});
+	
+	client = {};
+
+	if (regRes.statusCode == 204) {
+		res.render('index', {access_token: access_token, refresh_token: refresh_token, scope: scope, client: client});
+		return;
+	} else {
+		res.render('error', {error: 'Unable to delete client ' + regRes.statusCode});
+		return;
+	}
 	
 });
 
