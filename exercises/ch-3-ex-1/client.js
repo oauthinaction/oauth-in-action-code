6da9a00f7deb45fd3734c5bf23c1e23e893d0775
xuheyao <xuheyao@bytedance.com>
2020-02-14 00:12:08 +0800

    feat: 3.1 3.2.1
    Change-Id: I29adc272bcb4e072fe92974fb85b976137e20fb4

--- exercises/ch-3-ex-1/client.js
+++ exercises/ch-3-ex-1/client.js
@@ -16,8 +16,8 @@ app.set('views', 'files/client');
 
 // authorization server information
 var authServer = {
-	authorizationEndpoint: 'http://localhost:9001/authorize',
-	tokenEndpoint: 'http://localhost:9001/token'
+    authorizationEndpoint: 'http://localhost:9001/authorize',
+    tokenEndpoint: 'http://localhost:9001/token'
 };
 
 // client information
@@ -27,9 +27,9 @@ var authServer = {
  * Add the client information in here
  */
 var client = {
-	"client_id": "",
-	"client_secret": "",
-	"redirect_uris": ["http://localhost:9000/callback"]
+    "client_id": "oauth-client-1",
+    "client_secret": "oauth-client-secret-1",
+    "redirect_uris": ["http://localhost:9000/callback"]
 };
 
 var protectedResource = 'http://localhost:9002/resource';
@@ -40,58 +40,59 @@ var access_token = null;
 var scope = null;
 
 app.get('/', function (req, res) {
-	res.render('index', {access_token: access_token, scope: scope});
+    res.render('index', {access_token: access_token, scope: scope});
 });
 
-app.get('/authorize', function(req, res){
-
-	/*
-	 * Send the user to the authorization server
-	 */
-	
+app.get('/authorize', function (req, res) {
+    res.redirect(authorizedUrl);
 });
 
-app.get('/callback', function(req, res){
+app.get('/callback', function (req, res) {
 
-	/*
-	 * Parse the response from the authorization server and get a token
-	 */
-	
+    /*
+     * Parse the response from the authorization server and get a token
+     */
+
 });
 
-app.get('/fetch_resource', function(req, res) {
+app.get('/fetch_resource', function (req, res) {
 
-	/*
-	 * Use the access token to call the resource server
-	 */
-	
+    /*
+     * Use the access token to call the resource server
+     */
+
 });
 
-var buildUrl = function(base, options, hash) {
-	var newUrl = url.parse(base, true);
-	delete newUrl.search;
-	if (!newUrl.query) {
-		newUrl.query = {};
-	}
-	__.each(options, function(value, key, list) {
-		newUrl.query[key] = value;
-	});
-	if (hash) {
-		newUrl.hash = hash;
-	}
-	
-	return url.format(newUrl);
+var buildUrl = function (base, options, hash) {
+    var newUrl = url.parse(base, true);
+    delete newUrl.search;
+    if (!newUrl.query) {
+        newUrl.query = {};
+    }
+    __.each(options, function (value, key, list) {
+        newUrl.query[key] = value;
+    });
+    if (hash) {
+        newUrl.hash = hash;
+    }
+
+    return url.format(newUrl);
 };
 
-var encodeClientCredentials = function(clientId, clientSecret) {
-	return new Buffer(querystring.escape(clientId) + ':' + querystring.escape(clientSecret)).toString('base64');
+var encodeClientCredentials = function (clientId, clientSecret) {
+    return new Buffer(querystring.escape(clientId) + ':' + querystring.escape(clientSecret)).toString('base64');
 };
 
 app.use('/', express.static('files/client'));
 
 var server = app.listen(9000, 'localhost', function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('OAuth Client is listening at http://%s:%s', host, port);
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('OAuth Client is listening at http://%s:%s', host, port);
 });
- 
+
+var authorizedUrl = buildUrl(authServer.authorizationEndpoint, {
+    response_type: 'code',
+    client_id: client.client_id,
+    redirect_uri: client.redirect_uris[0]
+});
